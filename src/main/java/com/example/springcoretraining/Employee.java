53616c97e405f68f8c9be93dd9eb3ade6455d345
TriumphErmola <xramovic@yandex.ru>
2022-12-12 21:24:11 +0600

    Initial commit

---
+++ src/main/java/com/example/springcoretraining/Employee.java
@@ -0,0 +1,2 @@
+package com.example.springcoretraining;public class Employee {
+}
---
+++ src/main/java/com/example/springcoretraining/Welder.java
@@ -0,0 +1,17 @@
+package com.example.springcoretraining;
+
+public class Cat {
+
+    private String name;
+    private int age;
+
+    public Cat() {
+        System.out.println("Котец создался");
+    }
+
+    public void sayYourInfo(){
+        System.out.println("Я не умею создавать нормальные методы. увы");
+    }
+
+
+}
